2dd25f5ff95e94cdf435aa3971f192c1a65aa212
itays123 <itays2005@gmail.com>
2022-04-10 21:42:07 +0300

    FEATURE: Fonts and colors

--- habitz-ui/tailwind.config.js
+++ habitz-ui/tailwind.config.js
@@ -1,10 +1,25 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary: colors.purple,
+      secondary: colors.fuchsia,
+      accent: colors.teal,
+      base: colors.slate,
+    },
+    fontFamily: {
+      sans: ['"Lato"', ...defaultTheme.fontFamily.sans],
+      title: ['"Fredoka One"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
